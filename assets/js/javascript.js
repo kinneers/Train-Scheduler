81bec73e69a998205eb2fb4fea7dfff778971184
Sarah Kinneer <kinneers@gmail.com>
2019-01-06 19:30:05 -0500

    Initial commit of starter files

---
+++ assets/js/javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyA2RtL6OApQdUUwwkA6-AVjzesIGmp2E2Q",
+        authDomain: "amazing-train-scheduler.firebaseapp.com",
+        databaseURL: "https://amazing-train-scheduler.firebaseio.com",
+        projectId: "amazing-train-scheduler",
+        storageBucket: "",
+        messagingSenderId: "1010701429657"
+    };
+    
+    firebase.initializeApp(config);
+})
