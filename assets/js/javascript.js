29ba982015222ca8804ff8884dad72375c1282b1
Sarah Kinneer <kinneers@gmail.com>
2019-01-09 18:46:48 -0500

    Added remove button, pushing to test functionality with https request for partial page load

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,17 @@
+/*
+Developer: Sarah Kinneer
+Title: Trivia Game
+Description: Train Scheduler (JavaScript, JQuery, Moment.js, Firebase)
+Date: 1/6/2019
+
+Facts:
+The Orient Express took 7 days to get to Istanbul and 7 days to return to Paris, so it could 
+have departed every 20160 minutes (provided, of course that only one train 
+traveled the route at a given time and never required maintenance).
+Harry Potter did, in fact, leave from Platform 9 and 3/4 on the Hogwarts Express at 11:00.
+The Little Engine that Could definitely made it to the top of the mountian.
+*/
+
 $(document).ready(function() {
     // Initialize Firebase
     var config = {
@@ -13,13 +27,14 @@ $(document).ready(function() {
     //Set database reference
     var database = firebase.database();
 
-    //Initial Values
+    //Initial Values/Global Variables
     var trainName;
     var destination;
     var firstTrainTime;
     var frequency;
     var nextTrain;
     var minutesAway;
+    var clickedItem;
 
     function calculateValues(trainTime, freq) {
         /*This function uses the first train time and frequency to calculate the next arrival time and
@@ -66,43 +81,76 @@ $(document).ready(function() {
             frequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
+
+        //Clear form fields
+        $('#trainName').val('');
+        $('#destination').val('');
+        $('#firstTrainTime').val('');
+        $('#frequency').val('');
     });
 
     // Firebase watcher + initial loader
     database.ref().on("child_added", function(childSnapshot) {
-
+        console.log(childSnapshot.key);
         //Function to calculate new arrival times and minutes away
         calculateValues(childSnapshot.val().firstTrainTime, childSnapshot.val().frequency);
 
         //Appends current values to the page and adds update and remove buttons
         $('#well').append(
-            `<tr id="${childSnapshot.val().trainName}">
+            `<tr id="${childSnapshot.key}">
                 <td>${childSnapshot.val().trainName}</td>
                 <td>${childSnapshot.val().destination}</td>
                 <td>${childSnapshot.val().frequency}</td>
                 <td>${nextTrain}</td>
                 <td>${minutesAway}</td>
-                <td><button id="${childSnapshot.val().trainName}" class="update btn btn-primary">Update</button><td>
-                <td><button id="${childSnapshot.val().trainName}" class="remove btn btn-danger">Remove</button><td>
+                <td><button id="update" value="${childSnapshot.key}" class="update btn btn-primary">Update</button><td>
+                <td><button id="remove" value="${childSnapshot.key}" class="remove btn btn-danger">Remove</button><td>
             <tr>`
-            /*Note that the button ids for update and remove and tr are all the same- this needs to be corrected*/
         )
     // Handle the errors
     }, function(errorObject) {
         console.log("Errors handled: " + errorObject.code);    
     });
 
-    //Adds an event listener to the parent of the update and remove buttons
+    //Adds an event listener to the parent of the update and remove buttons (needed since they were created dynamically)
     var theParent = document.querySelector("#well");
     theParent.addEventListener("click", findButton, false);
+    //Function to determine which of the child buttons was clicked
+    function findButton(event) {
+        if (event.target !== event.currentTarget) {
+            clickedItem = event.target;
+            if (clickedItem.id === 'update') {
+                console.log("Update " + clickedItem.value);    
+                //Function to Update Field
+            }
+            else if (clickedItem.id === 'remove') {
+                console.log(clickedItem);
+                database.ref(clickedItem.value).remove();
+                $('#well').load('https://kinneers.github.io/Train-Scheduler.index.html #well');
 
-    function findButton(e) {
-        if (e.target !== e.currentTarget) {
-            var clickedItem = e.target;
-            console.log(clickedItem);
+            }
+            else {
+                console.log ("Error");
+            }
         }
-    e.stopPropagation();
+    event.stopPropagation();
     }
+
+
+    //Repopulates the chart when data is updated or removed from the database
+    database.ref().on('child_removed', function(childSnapshot) {
+        console.log(childSnapshot);
+        //$('#well').load('https://kinneers.github.io/Train-Scheduler.index.html #well');
+        //var removalId = childSnapshot.
+
+        //==$('#well').remove(childSnapshot);
+        
+    // Handle the errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);    
+    });
+
+
     /*
     ADAPT TO PUT THE ROWS IN CHRONOLOGICAL ORDER
     dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
