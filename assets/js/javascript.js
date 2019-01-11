caef08f7df6c26caddac7da32e07fadcca2c7d65
Sarah Kinneer <kinneers@gmail.com>
2019-01-11 18:37:39 -0500

    Added input validation, remove button functions properly, footer redesigned

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,10 +6,15 @@ Date: 1/6/2019
 
 Facts:
 The Orient Express took 7 days to get to Istanbul and 7 days to return to Paris, so it could 
-have departed every 20160 minutes (provided, of course that only one train 
-traveled the route at a given time and never required maintenance).
+have departed every 20160 minutes (provided, of course that only one train traveled the route 
+at a given time and never required maintenance).
+
 Harry Potter did, in fact, leave from Platform 9 and 3/4 on the Hogwarts Express at 11:00.
-The Little Engine that Could definitely made it to the top of the mountian.
+
+Spoiler Alert: The Little Engine that Could DOES make it to the top of the mountian.
+
+The Polar Express' Frequency is actually every 525600 minutes... but the frequency given was
+calculated in order for it to arrive Christmas Eve.
 */
 
 $(document).ready(function() {
@@ -27,14 +32,45 @@ $(document).ready(function() {
     //Set database reference
     var database = firebase.database();
 
+    /*  ATTEMPTING TO AUTHORIZE USERS
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+    */
+
     //Initial Values/Global Variables
-    var trainName;
-    var destination;
+    var trainName = '';
+    var destination = '';
     var firstTrainTime;
-    var frequency;
+    var frequency = 0;
     var nextTrain;
     var minutesAway;
     var clickedItem;
+    var regExTime = /^([01]\d|2[0-3]):?([0-5]\d)$/;
 
     function calculateValues(trainTime, freq) {
         /*This function uses the first train time and frequency to calculate the next arrival time and
@@ -53,45 +89,61 @@ $(document).ready(function() {
         minutesAway = freq - tRemainder;
         //Arrival time of next train
         nextTrain = moment().add(minutesAway, "minutes");
-        nextTrain = moment(nextTrain._d).format('HH:mm');
+        nextTrain = moment(nextTrain._d).format('MM/DD/YY @ hh:mm a');
     }
 
-    //Code to refresh the table every minute
-    setTimeout(function(){
-        $( "#well" ).load("https://kinneers.github.io/Train-Scheduler/ #well");
-     }, 60000); //refresh every minute
+    //ATTENOTUNG TO UPDATE PAGE EACH MINUTE (NOT WORKING... YET)
+    //Interval to refresh page data
+    //setInterval(refreshTable, 60000);
+
+    //database.ref().on("value", refreshTable(childSnapshot) {
+
+    //    calculateValues(latestSnapshot.val().firstTrainTime, latestSnapshot.val().frequency);
+      //  console.log("check for changes");
+    //}
+    
 
     //Capture Submit Button
     $('#addTrain').on('click', function(event) {
         event.preventDefault();
-
-        //LATER (IF TIME)- ADD IN FORM VALIDATION
-        //Regex for form validation of military time:   ^([01]\d|2[0-3]):?([0-5]\d)$
-        //https://mdbootstrap.com/docs/jquery/forms/validation/
 
         //Set the values for the most recent train added
         trainName = $('#trainName').val().trim();
         destination = $('#destination').val().trim();
         firstTrainTime = $('#firstTrainTime').val().trim();
         frequency = $('#frequency').val().trim();
-        
-        //Function to calculate arrival time and minutes away
-        calculateValues(firstTrainTime, frequency);
-        
-        //Push new train to the Firebase Database
-        database.ref().push({
-            trainName,
-            destination,
-            firstTrainTime,
-            frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
 
-        //Clear form fields
-        $('#trainName').val('');
-        $('#destination').val('');
-        $('#firstTrainTime').val('');
-        $('#frequency').val('');
+        //Validate inputs
+        if ((trainName !== '') && (destination !== '') && (regExTime.test(firstTrainTime)) && (frequency !== 0)) {
+            //Function to calculate arrival time and minutes away
+            calculateValues(firstTrainTime, frequency);
+                    
+            //Push new train to the Firebase Database
+            database.ref().push({
+                trainName,
+                destination,
+                firstTrainTime,
+                frequency,
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            });
+
+            //Clear form fields
+            $('#trainName').val('');
+            $('#destination').val('');
+            $('#firstTrainTime').val('');
+            $('#frequency').val('');
+        }
+        else {
+            //Add a modal to indicate that a field needs valid input
+
+
+
+
+
+
+            
+            console.log("Error");
+        }
     });
 
     // Firebase watcher + initial loader
@@ -108,7 +160,7 @@ $(document).ready(function() {
                 <td>${childSnapshot.val().frequency}</td>
                 <td>${nextTrain}</td>
                 <td>${minutesAway}</td>
-                <td><button id="update" value="${childSnapshot.key}" class="update btn btn-primary">Update</button><td>
+                <td><button id="update" type="button" value="${childSnapshot.key}" class="update btn btn-primary">Update</button><td>
                 <td><button id="remove" value="${childSnapshot.key}" class="remove btn btn-danger">Remove</button><td>
             <tr>`
         )
@@ -125,11 +177,11 @@ $(document).ready(function() {
         if (event.target !== event.currentTarget) {
             clickedItem = event.target;
             if (clickedItem.id === 'update') {
-                console.log("Update " + clickedItem.value);    
-                //Function to Update Field
+                console.log("Update " + clickedItem.value);
+                //Add Function to Update Field
+
             }
             else if (clickedItem.id === 'remove') {
-                console.log(clickedItem);
                 database.ref(clickedItem.value).remove();
             }
             else {
@@ -147,15 +199,4 @@ $(document).ready(function() {
     }, function(errorObject) {
         console.log("Errors handled: " + errorObject.code);    
     });
-
-    /*
-    ADAPT TO PUT THE ROWS IN CHRONOLOGICAL ORDER
-    dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-    // Change the HTML to reflect
-    $("#name-display").text(snapshot.val().name);
-    $("#email-display").text(snapshot.val().email);
-    $("#age-display").text(snapshot.val().age);
-    $("#comment-display").text(snapshot.val().comment);
-    })
-    */
 });
